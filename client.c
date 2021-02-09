81d640c3ae5d900afe71f2e1e3a5aeefc9c2cb20
Adithya-S-Bhat <56101205+Adithya-S-Bhat@users.noreply.github.com>
2021-02-09 15:57:06 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include <string.h> 
+#include <ctype.h>
+#include"server.h"
+void main(){
+    clrscr();
+    home();
+}
---
+++ server.h
@@ -0,0 +1,24 @@
+struct user
+{
+char firstname[30];
+char lastname[30];
+char username[30]; 
+char password[30];
+double balance; 
+int accno; 
+};
+struct dbase
+{
+    char firstname[30];
+    char lastname[30];
+    char username[30]; 
+    char password[30];
+    double balance; 
+    int accno; 
+};
+struct user userData;
+void clrscr();
+void home();
+void login(FILE *db,int size);
+int selectoption(char *userName);
+void createacc(FILE *db,int size);
