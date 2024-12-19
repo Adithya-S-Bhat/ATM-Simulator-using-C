c9073333ec30ce5b853fd2c158c12dc67827ffed
rain <xxraincandyxx@gmail.com>
2024-12-19 22:04:45 +0800

    add functinal file

--- pr.mk
+++ pr.mk
@@ -1,5 +1,5 @@
-ATM.exe: client.o server.o admin.o
-	gcc client.o server.o admin.o -o ATM.exe
+ATM.exe: client.o server.o admin.o functional.o
+	gcc client.o server.o admin.o functional.o -o ATM.exe
 
 client.o: client.c server.h
 	gcc -c client.c
@@ -9,3 +9,6 @@ server.o: server.c server.h
 
 admin.o: admin.c server.h
 	gcc -c admin.c
+
+functional.o: functional.c server.h
+	gcc -c functional.c
